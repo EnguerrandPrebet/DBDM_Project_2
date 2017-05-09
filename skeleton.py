819f6d6afdb535079866e392b33cb14a94de2289
Remi Coudert <remi.coudert@epfl.ch>
2017-05-09 18:05:08 +0200

    skeleton

--- skeleton.py
+++ skeleton.py
@@ -10,6 +10,19 @@ matches = main_file[((main_file['Year'] == 2017) | (main_file['Year'] == 2016))
                     & ((main_file['League'] == 'Europe') |
                     (main_file['League'] == 'North_America') |
                     (main_file['League'] == 'LCK')
-                    )].loc[:, 'blueTop':'goldblueSupport']
+                    )].drop(['blueTop', 'blueTopChamp', 'goldblueTop', 'blueJungle', 'blueJungleChamp', 'goldblueJungle',
+              'blueMiddle', 'blueMiddleChamp', 'goldblueMiddle', 'blueADC', 'blueADCChamp',
+              'goldblueADC', 'blueSupport', 'blueSupportChamp', 'goldblueSupport',
+              'redTop', 'redTopChamp', 'goldredTop', 'redJungle', 'redJungleChamp', 'goldredJungle',
+              'redMiddle', 'redMiddleChamp', 'goldredMiddle', 'redADC', 'redADCChamp',
+              'goldredADC', 'redSupport', 'redSupportChamp', 'goldredSupport'
+              ], axis=1)
 
-print(matches.head())
+
+
+#matches.to_csv('test.csv')
+
+x = gold[gold.NameType == 'golddiff'].describe().drop['std', 'count', '25%', '50%', '75%', 'max']
+x.plot.hist()
+
+plt.show()
